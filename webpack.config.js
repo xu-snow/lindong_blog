cefc88e9bb4b515d582184a54cab64b9ce858c31
zhengxu <332171564@qq.com>
2017-09-16 12:27:27 +0800

    update
    Signed-off-by: zhengxu <332171564@qq.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,86 +2,86 @@ var path = require('path')
 var webpack = require('webpack')
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir)
 }
 
 
 module.exports = {
-  entry: {
-    main: './src/main.js',
-    vendor: ['jquery', './src/req/index.js']
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: '[name].build.js'
-  },
-  resolveLoader: {
-    modules: [path.join(__dirname, 'node_modules')],
-  },
-  module: {
-    loaders: [{
-        test: /\.vue$/,
-        use: ["vue-loader"],
-        include: [resolve('src')]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-       
-      },
-      {
-        test: /\.js$/,
-        use: ["babel-loader"],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-        use: [{
-          loader: "url-loader",
-          query: {
-            limit: 50000,
-            name: '[name].[ext]?[hash]'
-          }
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor'],
-      minChunks: 2
-    })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    //noInfo: false,
-    proxy:{
-      "/resource/*":"http://138.68.235.110"
-    }
-  },
-  devtool: '#eval-source-map'
+    entry: {
+        main: './src/main.js',
+        vendor: ['jquery', './src/req/index.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: '[name].build.js'
+    },
+    resolveLoader: {
+        modules: [path.join(__dirname, 'node_modules')],
+    },
+    module: {
+        loaders: [{
+                test: /\.vue$/,
+                use: ["vue-loader"],
+                include: [resolve('src')]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                use: [{
+                    loader: "url-loader",
+                    query: {
+                        limit: 50000,
+                        name: '[name].[ext]?[hash]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor'],
+            minChunks: 2
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        //noInfo: false,
+        // proxy:{
+        //   "/resource/*":"http://138.68.235.110"
+        // }
+    },
+    devtool: '#eval-source-map'
 }
 
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ])
+    module.exports.devtool = '#source-map'
+        // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
 }
